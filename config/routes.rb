9f65350cbf4122b026c1a2e98d456da6b152e659
James Adney <jfadney@gmail.com>
2013-06-05 21:08:00 -0700

    redirect root to tickets index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Helpdesk::Application.routes.draw do
   resources :tickets
 
+  root to: redirect("/tickets")
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
