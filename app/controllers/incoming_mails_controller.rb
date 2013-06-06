342c4bfbb4b97b1a3e5dcfbf5443729e5a9f2d45
Ivan Storck <ivanoats@gmail.com>
2013-06-06 09:46:48 -0700

    raising params

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -2,7 +2,8 @@ class IncomingMailsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    Rails.logger.log params[:subject]
+    # Rails.logger.log params[:headers][:Subject]
+    raise params.inspect
     render :text => 'success', :status => 200 # a status of 404 would reject the mail
   end
 end
