f58567dfdacf99298c29ec4518f8d7bc76a48be0
James Adney <jfadney@gmail.com>
2013-06-05 23:18:35 -0700

    try not doing anything with the email

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -2,8 +2,6 @@ class IncomingMailsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    Rails.logger.log params[:plain]
-
     render :text => 'success', :status => 200 # a status of 404 would reject the mail
   end
 end
