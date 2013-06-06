60991fa8e870ec2db6f09dc912df253ef082a9ca
James Adney <jfadney@gmail.com>
2013-06-05 20:18:14 -0700

    base with postgres, unicorn, procfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helpdesk::Application.config.secret_token = '4405b284845886ddb07d5e95eacb775ef0e456d9f64b08f5ba84aadb4a5b49888cadd6f2878688283b5d1a8c322ee9f2ab115652594cca42ccbaac7f8c65cf93'
